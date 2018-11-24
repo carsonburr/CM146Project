8457e7608fb32a56e054d5073644723c51e27407
Michael Hsieh <mdhsieh@ucsc.edu>
2018-11-23 18:39:57 -0800

    added 8-way rotation, sprite change
    player faces general mouse direction, animates, changed player sprite

--- Player.gd
+++ Player.gd
@@ -4,6 +4,9 @@ extends Area2D
 Using temp player images, mage-f-32x36-sprite-png,
 from https://opengameart.org/content/antifareas-rpg-sprite-set-1-enlarged-w-transparent-background-fixed
 by Antifarea.
+purple-f-32x32-sprite-png/,
+from http://himeworks.com/2014/11/introducing-diagonal-movement-into-rpg-maker/
+by HIME.
 
 Online sprite sheet cutter: https://ezgif.com/sprite-cutter/ezgif-4-4210e31654a4.png
 Had to use cutting by tile size.
@@ -55,6 +58,46 @@ func _process(delta):
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
 	
+	
+	var mouse_pos = get_global_mouse_position()
+	
+	# convert from rad to deg w/ rad2deg() or vice versa w/ deg2rad()
+	var deg_angle = rad2deg($AnimatedSprite.get_angle_to(mouse_pos))
+	
+	# character starts by facing right = 0 degrees
+	# right = 0, up = -90, left = 179 or -179, down = 90
+	# face the general direction of the mouse
+	
+	# 8-way rotation animation
+	if deg_angle > -30 and deg_angle < 30:
+		$AnimatedSprite.animation = "right"
+	elif deg_angle >= -60 and deg_angle <= -30:
+		$AnimatedSprite.animation = "up-right"
+	elif deg_angle < -60 and deg_angle > -120:
+		$AnimatedSprite.animation = "up"
+	elif deg_angle >= -150 and deg_angle <= -120:
+		$AnimatedSprite.animation = "up-left"
+	elif (deg_angle < -150 and deg_angle > -180) or (deg_angle > 150 and deg_angle < 180):
+		$AnimatedSprite.animation = "left"
+	elif deg_angle >= 30 and deg_angle <= 60:
+		$AnimatedSprite.animation = "down-right"
+	elif deg_angle > 60 and deg_angle < 120:
+		$AnimatedSprite.animation = "down"
+	elif deg_angle >= 120 and deg_angle <= 150:
+		$AnimatedSprite.animation = "down-left"
+	
+	"""
+	# 4-way rotation animation
+	if deg_angle > -45 and deg_angle < 45:
+		$AnimatedSprite.animation = "right"
+	elif deg_angle <= -45 and deg_angle > -135:
+		$AnimatedSprite.animation = "up"
+	elif (deg_angle <= -135 and deg_angle > -180) or (deg_angle >= 135 and deg_angle < 180):
+		$AnimatedSprite.animation = "left"
+	elif deg_angle >= 45 and deg_angle < 135:
+		$AnimatedSprite.animation = "down"
+	
+	# normal walking animation
 	if velocity.x > 0:
 		$AnimatedSprite.animation = "right"
 	elif velocity.x < 0:
@@ -63,6 +106,7 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 	elif velocity.y > 0:
 		$AnimatedSprite.animation = "down"
+	"""
 		
 	if Input.is_action_pressed("click"):
 		shoot()
@@ -70,11 +114,10 @@ func _process(delta):
 	control(delta)
 		
 func control(delta):
-	# normally would be ex. the turret of a tank not AnimatedSprite
+	# normally would be ex. the turret of a tank not BulletSpawnPoint or AnimatedSprite
 	# $AnimatedSprite.look_at(get_global_mouse_position())
 	$BulletSpawnPoint.look_at(get_global_mouse_position())
 	$BulletSpawnPoint.global_position = position
-	pass
 		
 func shoot():
 	if can_shoot:
