8374f3f2f3dc66f6c06305bfe44ec112458780c3
Michael Hsieh <mdhsieh@ucsc.edu>
2018-12-08 11:06:42 -0800

    offset beam, added rotation
    beam init firing = shooting, beam held = final beam = last frame of shooting

--- Laser.gd
+++ Laser.gd
@@ -4,19 +4,28 @@ extends "res://Bullet.gd"
 
 # extends Area2D
 
+# inherited Sprite and velocity unused
 # var velocity = Vector2()
+
+var screensize
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	speed = 0
 	
+	screensize = get_viewport_rect().size
 	# arbitrary position, can change later
-	# (250, 250) set in inspector
 	# boss positon: (screensize.x / 2, screensize.y / 2 - 100)
+	position.x = screensize.x / 2
+	position.y = screensize.y / 2 - 100
+	
+	# speed in frames per second
+	$AnimatedSprite.frames.set_animation_speed("shooting", 10)
+	#print($AnimatedSprite.frames.get_animation_speed("shooting"))
 
 func start(_position, _direction):
-	position = _position
+	# position = _position
 	rotation = _direction.angle()
 	$Lifetime.wait_time = lifetime
 	velocity = _direction * speed
@@ -24,6 +33,12 @@ func start(_position, _direction):
 func _process(delta):
 	# position += velocity * delta
 	$AnimatedSprite.play()
+	# PI / 6 = 30 deg / 6 = 5 deg
+	rotation = rotation + PI / 36
+	
+	if $AnimatedSprite.animation == "shooting" and $AnimatedSprite.frame == 5:
+		$AnimatedSprite.stop()
+		# $AnimatedSprite.animation = "final beam"
 	
 func explode():
 	queue_free()
